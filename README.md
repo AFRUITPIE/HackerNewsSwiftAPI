4d86d902c152578fc56e82dc04a12480befe99b4
Hayden Hong <hello@haydenhong.com>
2023-03-25 21:24:56 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# HackerNews-Swift-API
+
+A description of this package.
---
+++ Sources/HackerNews-Swift-API/HackerNews_Swift_API.swift
@@ -0,0 +1,6 @@
+public struct HackerNews_Swift_API {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
